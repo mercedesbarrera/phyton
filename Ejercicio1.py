d220f2383e3de541a6c7bed3fa7f5c5556ec3247
mercedesbcbc@gmail.com <mercedesbcbc@gmail.com>
2025-10-22 09:43:41 +0200

    primer commit python

---
+++ Ejercicio1.py
@@ -0,0 +1,107 @@
+"""EPOOO1
+print("Soy un programa en Python")P
+"""
+"""EP0002
+valor1=90
+valor2=200
+valor3=5
+suma=valor1+valor2+valor3
+resta=valor1-valor3
+multiplica=valor2*valor3
+divide=valor2/valor3
+print(f"la suma es:{suma}")
+print(f"la resta es: {resta}")
+print(f"la multiplicación es: {multiplica}")
+print(f"la division es:{divide}")"""
+
+"""EP0003
+print(-8+6*3)
+print((2+7)%5)
+print(2+(-1*8)-5 % 3)"""
+"""EP0004
+valor1=-8+6*3
+valor2=(2+7)%5
+valor3=(2+(-1*8)-5%3)
+print(f"El valor 1 es: {valor1}")
+print(f"El valor 2 es: {valor2}")
+print(f"El valor 3 es: {valor3}")"""
+"""EP0005 no se puede realizar por que la variable empieza por numero"""
+"""EP0006
+entero1=321
+entero2=698
+resultado=entero1/entero2
+print(f"El resultado es: {resultado:.3f}")"""
+"""EP00012
+numA=129070
+numB=20689
+resultado=numA*numB
+print(f"El resultado es: {resultado}")"""
+"""EP0085"""
+"""Inicializamos a
+a=3"""
+"""++a pre-increment
+a+=1
+b=a
+
+c=a
+a+=1
+
+b=b*3
+a=a*2
+print(f"a= {a}")
+print(f"b= {b}")
+print(f"c= {c}")"""
+"""Leyendo de consola. Tipos"""
+"""EP0007
+nombre=input("Introduce un nombre:")
+print(f"Hola, {nombre}")"""
+"""EP0008
+numero1=int(input("Introduce un numero:"))
+numero2=int(input("Introduce otro numero:"))
+resultado=numero1+numero2
+print(f"El resultado es: {resultado}")"""
+"""EP0009
+metros=float(input("Introduce una distancia en metros: "))
+resultado=metros/1609
+print(f"El resultado es: {resultado}")"""
+"""EP0010
+n1=int(input("Introduce un numero: "))
+n2=int(input("Introduce otro numero: "))
+division=n1/n2
+resto=n1%n2
+print(f"El resultado de la division es: {division}")
+print(f"El resultado de la resto es: {resto}")"""
+"""EP0011
+num=int(input("Introduce un numero:"))
+print(num*1)
+print(num*2)
+print(num*3)
+print(num*4)
+print(num*5)
+print(num*6)
+print(num*7)
+print(num*8)
+print(num*9)
+print(num*10)"""
+"""EP0059
+precio=float(input("Introduce un precio sin IVA: "))
+precioFinal=precio*1.21
+print(f"El resultado es: {precioFinal}")"""
+"""EP0058
+n1=int(input("Introduce un numero:"))
+n1=abs(n1)
+n1=n1%10
+print(f"El resultado es: {n1}")"""
+"""EP0088
+r = int(input("Rojo (0-255): "))
+g = int(input("Verde (0-255): "))
+b = int(input("Azul (0-255): "))
+
+print(f"El color en hexadecimal es: #{r:02X}{g:02X}{b:02X}")"""
+
+
+
+
+
+
+
