037563378694492ee6e44c85e67d24faa39537cd
mercedesbcbc@gmail.com <mercedesbcbc@gmail.com>
2025-10-27 13:16:49 +0100

    gg

--- EjerciciosNuevosPython.py
+++ EjerciciosNuevosPython.py
@@ -105,3 +105,4 @@ for letra in cadena:
         print(f"La letra {letra} aparece {contador} veces")
         # Agregamos la letra a la lista de letras ya contadas
         ocurrencias.append(letra)
+edrftgyhujihugyf
